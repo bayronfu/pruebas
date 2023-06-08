abb07b8fb17314e8549da7bed0767b74a4205bc5
bayronfu <bayr.fuentes@duocuc.cl>
2023-06-08 18:05:08 -0400

    hii

---
+++ ontour/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('index',views.index,name='index'),
+    path('actualizar', views.actualizar, name='actualizar'),
+    path('Asia', views.Asia, name='Asia'),
+    path('carga', views.carga, name='carga'),
+    path('Chile', views.Chile, name='Chile'),
+    path('contacto', views.contacto, name='contacto'),
+    path('crear', views.crear, name='crear'),
+    path('destinos', views.destinos, name='destinos'),
+    path('Europa', views.Europa, name='Europa'),
+    path('indexEjecutivo', views.indexEjecutivo, name='indexEjecutivo'),
+    path('inicioSesion', views.inicioSesion, name='inicioSesion'),
+    path('reporte', views.reporte, name='reporte'),
+    path('subir', views.subir, name='subir'),
+]
---
+++ ontour/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    context={"index":index}
+    return render(request, 'ontour/index.html', context)
+
+def actualizar(request):
+    context={}
+    return render(request, 'ontour/actualizar.html', context)
+
+def Asia(request):
+    context={}
+    return render(request, 'ontour/Asia.html', context)
+
+def carga(request):
+    context={}
+    return render(request, 'ontour/carga.html', context)
+
+def Chile(request):
+    context={}
+    return render(request, 'ontour/Chile.html', context)
+
+def contacto(request):
+    context={}
+    return render(request, 'ontour/contacto.html', context)
+
+def crear(request):
+    context={}
+    return render(request, 'ontour/crear.html', context)
+
+def destinos(request):
+    context={}
+    return render(request, 'ontour/destinos.html', context)
+
+def Europa(request):
+    context={}
+    return render(request, 'ontour/Europa.html', context)
+
+def indexEjecutivo(request):
+    context={}
+    return render(request, 'ontour/indexEjecutivo.html', context)
+
+def inicioSesion(request):
+    context={}
+    return render(request, 'ontour/inicioSesion.html', context)
+
+def reporte(request):
+    context={}
+    return render(request, 'ontour/reporte.html', context)
+
+def subir(request):
+    context={}
+    return render(request, 'ontour/subir.html', context)
